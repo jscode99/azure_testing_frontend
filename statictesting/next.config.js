47e6b766b6bdbab4f86aeee4757eccf6ad354877
msoultan <azmsoultan@outlook.com>
2021-10-26 16:39:08 +0200

    fix the config.

--- statictesting/next.config.js
+++ statictesting/next.config.js
@@ -1,11 +1,7 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
-  env: {
-    BASE_URL: "http://20.74.161.164:1337",
-    PATH: {
-      test: "/tests",
+    reactStrictMode: true,
+    images: {
+        loader: 'cloudinary',
     },
-  },
-  images: {
-    domains: ["20.74.161.164"],
-  },
 };
